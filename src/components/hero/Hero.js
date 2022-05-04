ae75b5a1a33150d59406e7db6e6523b4490238d5
anupmaharzn <hellen7203@gmail.com>
2022-05-04 16:01:06 +0545

    added hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import './hero.scss';
+import { image } from '../../images';
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div>
+            <div className="hero">
+
+                <div className='right-top-image'>
+                    <picture>
+                        <source media="(max-width:767px)"
+                            srcSet={image.intro_right_mobile}
+                        />
+                        <img src={image.intro_right} alt="" />
+                    </picture>
+                </div>
+
+                <div className='hero__wrapper container'>
+
+                    <div className='hero__content'>
+                        <h1 className='title1'>
+                            Humanizing<br />your insurance.
+                        </h1>
+                        <p className='hero__text'>
+                            Get your life insurance coverage easier and faster. We blend our expertise
+                            and technology to help you find the plan that’s right for you. Ensure you
+                            and your loved ones are protected.
+                        </p>
+                        <button className='btn'>
+                            VIEW PLANS
+                        </button>
+                    </div>
+
+                    <div className='hero__image'>
+                        <picture>
+                            <source media="(max-width:767px)"
+                                srcSet={image.intro_mobile}
+                            />
+                            <img src={image.intro_desktop} alt="" />
+                        </picture>
+                    </div>
+
+                </div>
+
+                <div className='left-bottom-image'>
+                    <picture>
+                        <source media="(max-width:767px)"
+                            srcSet={image.intro_left_mobile}
+                        />
+                        <img src={image.intro_left} alt="" />
+                    </picture>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
