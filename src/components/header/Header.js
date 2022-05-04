71c1cb58ca86556bd6d2c630988dfce9b3191107
anupmaharzn <hellen7203@gmail.com>
2022-05-04 13:32:50 +0545

    added header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import { image } from '../../images';
 const Header = () => {
+    const [Open, setOpen] = useState(false);
+    const handleClick = (e) => {
+        e.preventDefault();
+        setOpen(!Open);
+    }
     return (
-        <header>
-            <nav>
-                <div className='logo'>
-                    <img src={image.logo} alt="logo"></img>
-                </div>
-            </nav>
-        </header>
+        <div>
+            <header>
+
+                <nav className='navbar container'>
+                    <div className='logo'>
+                        <img src={image.logo} alt="logo" />
+                    </div>
+                    <ul className={Open ? `nav-items active` : `nav-items`}>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Portfolio</li>
+                        <li className='btn btn--nav-btn'>View Plans </li>
+                    </ul>
+                    <div className="hamburger">
+                        <img src={image.hamburger} alt="hamburger" onClick={handleClick} />
+                    </div>
+                </nav>
+            </header>
+        </div>
     )
 }
 
